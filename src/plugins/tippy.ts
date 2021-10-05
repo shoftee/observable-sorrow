4de361f0113ff25e6504d00352674de76a6a6c1d
shoftee <shoftee@gmail.com>
2021-10-06 02:12:44 +0300

    enhance: add shift-away animation to tippy

--- src/plugins/tippy.ts
+++ src/plugins/tippy.ts
@@ -3,12 +3,14 @@ import { App } from "vue";
 import VueTippy from "vue-tippy";
 import "tippy.js/dist/tippy.css";
 import "tippy.js/themes/light.css";
+import "tippy.js/animations/shift-away.css";
 
 // Docs for Vue 3 Tippy here: https://vue-tippy.netlify.app/
 function applyTippy<TElement>(app: App<TElement>): App<TElement> {
   return app.use(VueTippy, {
     component: "tippy",
     defaultProps: {
+      animation: "shift-away",
       theme: "light",
       arrow: false,
       hideOnClick: false,
