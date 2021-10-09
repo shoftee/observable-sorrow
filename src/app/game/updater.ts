12c122f40d6d167fba331ba1e1870d049c60291d
shoftee <shoftee@gmail.com>
2021-10-10 00:38:58 +0300

    enhance: simplifying game updater code

--- src/app/game/updater.ts
+++ src/app/game/updater.ts
@@ -5,19 +5,16 @@ import { TimeConstants } from "../constants";
 type UpdateFunction = (deltaTime: number) => void;
 
 export class GameUpdater {
-  private readonly callback: UpdateFunction;
-  private readonly timestampProvider: ITimestamp;
-
   private lastTimestamp = 0;
   private handle?: number;
 
-  constructor(callback: UpdateFunction, timestampProvider: ITimestamp) {
-    this.callback = callback;
-    this.timestampProvider = timestampProvider;
-  }
+  constructor(
+    private readonly callback: UpdateFunction,
+    private readonly timestamp: ITimestamp,
+  ) {}
 
   start(): void {
-    this.lastTimestamp = this.timestampProvider.millis();
+    this.lastTimestamp = this.timestamp.millis();
 
     this.handle = setInterval(
       () => this.doUpdate(),
@@ -29,28 +26,8 @@ export class GameUpdater {
     this.cancel();
   }
 
-  /** Forces the game to update its internal state.
-   *
-   * NOTE: Use sparingly! If called because of UI events, throttle the events accordingly.
-   */
-  force(): boolean {
-    // ignore call if another update is expected soon
-    const margin = TimeConstants.MillisecondsPerUpdate * 0.1;
-    const expectedOn = this.lastTimestamp + TimeConstants.MillisecondsPerUpdate;
-    const cutoff = expectedOn - margin;
-
-    const now = this.timestampProvider.millis();
-    if (now > cutoff) {
-      return false;
-    }
-
-    this.doUpdate();
-
-    return true;
-  }
-
   private doUpdate(): void {
-    const now = this.timestampProvider.millis();
+    const now = this.timestamp.millis();
     const delta = Math.round(now - this.lastTimestamp);
     // ignore calls which happen impossibly soon (under 1ms)
     if (delta > 0) {
