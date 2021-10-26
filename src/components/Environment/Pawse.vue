4a0e9713aebf7d0cfce5ec2e51f80473037f6d90
shoftee <shoftee@gmail.com>
2021-10-26 15:21:39 +0300

    enhance: simplify Pawse component

--- src/components/Environment/Pawse.vue
+++ src/components/Environment/Pawse.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
+
+import { useI18n } from 'vue-i18n';
 import { injectChannel } from '@/composables/game-channel';
 
-const { interactors } = injectChannel();
+const { t } = useI18n();
+const { controller } = injectChannel().interactors;
+
 const paused = ref(false);
-const label = computed(() => paused.value ? "game.control.unpawse" : "game.control.pawse");
 async function pause() {
-  await interactors.controller.stop()
+  await controller.stop()
   paused.value = true
 }
 async function unpause() {
-  await interactors.controller.start()
+  await controller.start()
   paused.value = false;
 }
 </script>
 <template>
-  <button type="button" :class="{ active: paused }" @click="paused ? unpause() : pause()">
-    <i18n-t :keypath="label" scope="global"></i18n-t>
-  </button>
+  <button
+    type="button"
+    :class="{ active: paused }"
+    @click="paused ? unpause() : pause()"
+  >{{ t(paused ? "game.control.unpawse" : "game.control.pawse") }}</button>
 </template>
