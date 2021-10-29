6f6786037c4d9563168401f3d422b21e68e84413
shoftee <shoftee@gmail.com>
2021-10-29 04:02:16 +0300

    fix: test now mocks cursor correctly

--- tests/unit/store/migrations/v3.spec.ts
+++ tests/unit/store/migrations/v3.spec.ts
@@ -5,31 +5,58 @@ import { expect } from "chai";
 
 describe("migrateV3", () => {
   it("should fill unlocked field for buildings", async () => {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    await migrateV3(null!, {
-      objectStore: () => ({
-        getAll() {
-          return asPromise([
-            {
-              state: {
-                buildings: { "catnip-field": { level: 1 }, hut: { level: 0 } },
-              },
+    await migrateV3(
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      null!,
+      mockCursor({
+        load() {
+          return {
+            version: 1,
+            state: {
+              buildings: { "catnip-field": { level: 1 }, hut: { level: 0 } },
             },
-          ]);
+          } as SaveSlot;
         },
-        put(save: SaveSlot) {
+        save(save: SaveSlot) {
           const catnipField = (save.state.buildings ?? {})["catnip-field"];
           expect(catnipField?.level).to.equal(1);
           expect(catnipField?.unlocked).to.be.true;
+
           const hut = (save.state.buildings ?? {})["hut"];
           expect(hut?.level).to.equal(0);
           expect(hut?.unlocked).to.be.false;
         },
       }),
-    } as unknown as UpgradeTransaction);
+    );
   });
 });
 
 function asPromise<T>(value: T): Promise<T> {
   return new Promise((resolve) => resolve(value));
+}
+
+function mockCursor(handlers: {
+  load(): SaveSlot;
+  save(state: SaveSlot): void;
+}) {
+  return {
+    objectStore() {
+      return {
+        openCursor() {
+          return asPromise({
+            get value() {
+              return handlers.load();
+            },
+            continue() {
+              return asPromise(undefined);
+            },
+            update(s: SaveSlot) {
+              handlers.save(s);
+              return asPromise(undefined);
+            },
+          });
+        },
+      };
+    },
+  } as unknown as UpgradeTransaction;
 }
